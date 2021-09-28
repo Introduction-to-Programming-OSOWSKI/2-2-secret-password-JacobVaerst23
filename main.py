61aad3120d239a627d240fcca0f88bf31daec050
JacobVaerst23 <816769@mystma.org>
2021-09-28 09:09:08 -0500

    password attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define a function
+def password(p):
+    if p == "Knights19":
+        return "Access Granted"
+
+    else:
+        return "Access Denied"
+
+#Run the function
+print(password("Knights19"))
